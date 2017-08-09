8dee0726aa0150496bb36b524d03e2265f9885cc
lwkkazz <lwkkazz@users.noreply.github.com>
2017-08-09 16:03:04 -0300

    Add files via upload

---
+++ web_root/public/staff/subjects/show.php
@@ -0,0 +1,20 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
+<?php $id = $_GET['id'] ?? '255'; ?>
+
+<?php $page_title = 'Show Subject'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
+
+<div id="content">
+
+  <a class="back-link" href="<?php echo url_for('staff/subjects/index.php'); ?>">&laquo; Back to List</a>
+
+  <div class="subject show">
+
+    Subject ID: <?php echo h($id); ?>
+
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/staff_footer.php') ?>
