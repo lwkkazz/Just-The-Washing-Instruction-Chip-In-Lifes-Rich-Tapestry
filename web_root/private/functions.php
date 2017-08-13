f339ebee1711c42a868fd0dfd3395eb3dbf45ded
lwkkazz <lwkkazz@users.noreply.github.com>
2017-08-13 07:41:53 -0300

    Add files via upload

--- web_root/private/functions.php
+++ web_root/private/functions.php
@@ -33,5 +33,13 @@ function error_500() {
 function redirect_to($location) {
 	header("Location: " . $location);
   	exit;
-}	
+}
+
+function is_post_request(){
+  return ($_SERVER['REQUEST_METHOD']=='POST');
+}
+
+function is_get_request(){
+  return ($_SERVER['REQUEST_METHOD']=='GET');
+}
 ?>
